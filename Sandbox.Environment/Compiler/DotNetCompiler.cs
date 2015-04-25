89f20efd434d1513e6588324ebc8ad82335cebed
KTraczyk <traczyk92@gmail.com>
2015-04-25 15:57:58 +0200

    Wrapper & Compiler
    Added .Net Wrapper & Compiler

---
+++ Sandbox.Environment/Compiler/DotNetCompiler.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.Contracts;
+using Sandbox.Environment.Configuration;
+using Sandbox.Environment.Wrapper;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Reflection;
+
+namespace Sandbox.Environment.Compiler
+{
+    class DotNetCompiler : BinaryCompiler
+    {
+        protected override string SourceExtension { get { return "cs"; } }
+
+        protected override string LibraryExtension { get { return "dll"; } }
+
+        protected override string ExecutableExtension { get { return "exe"; } }
+
+
+        protected override IWrapper GetCodeWrapper(CompilerArgs args)
+        {
+            return new DotNetWrapper(args);
+        }
+
+        protected override void ImportLibraries()
+        {
+            foreach (string library in Args.Libraries)
+            {
+                ImportLibraryFile(library, library + ".dll");
+            }
+        }
+
+        protected override void MoveToTargetDirectory()
+        {
+            File.Move(Path.Combine(TemporaryDirectory, ExecutableFile),
+                Path.Combine(PackageDirectory, ExecutableFile));
+
+            foreach (string library in Args.Libraries)
+            {
+                File.Move(Path.Combine(TemporaryDirectory, library + ".dll"),
+                    Path.Combine(PackageDirectory, library + ".dll"));
+            }
+        }
+
+        protected void CompileSource(string sourceFilePath, string targetFile)
+        {
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            CompilerParameters parameters = new CompilerParameters();
+            //parameters.CompilerOptions = "/target:library";
+            parameters.GenerateExecutable = true;
+            parameters.OutputAssembly = targetFile + ".exe";
+            CompilerResults cr = codeProvider.CompileAssemblyFromFile(parameters, sourceFilePath);
+            if (cr.Errors.Count > 0)
+            {
+                // Display compilation errors.
+                Console.WriteLine("Errors building {0} into {1}",
+                    "exampleDll.cs", cr.PathToAssembly);
+                foreach (CompilerError ce in cr.Errors)
+                {
+                    Console.WriteLine("  {0}", ce.ToString());
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Source {0} built into {1} successfully.",
+                    sourceFilePath, cr.PathToAssembly);
+                Console.WriteLine("{0} temporary files created during the compilation.",
+                    parameters.TempFiles.Count.ToString());
+            }
+        }
+
+        public override void CompileLibrary(CompilerArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Sandbox.Environment/Wrapper/DotNetWrapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.Contracts.Code;
+using Sandbox.Environment.Compiler;
+using System.IO;
+
+namespace Sandbox.Environment.Wrapper
+{
+    class DotNetWrapper : IWrapper
+    {
+        private CompilerArgs _args;
+
+        public DotNetWrapper(CompilerArgs args)
+        {
+            _args = args;
+        }
+
+        public void ToStream(Stream stream)
+        {
+            using (TextWriter writer = new StreamWriter(stream))
+            {
+                writer.WriteLine(@"using System;");
+                writer.WriteLine(@"using System.Collections.Generic;");
+                writer.WriteLine(@"using System.Text;");
+                writer.WriteLine(@"using System.Threading.Tasks;");
+                
+                writer.WriteLine(@"namespace {0}", _args.PackageName);
+                writer.WriteLine(@"{");
+                writer.WriteLine(@"class Program");
+                writer.WriteLine(@"{");
+                writer.WriteLine(@"static public {0} resolve()", GetTypeRepresentation(_args.ReturnType));
+                writer.WriteLine(@"{");
+                writer.WriteLine(_args.Code);
+                writer.WriteLine(@"}");
+
+                writer.WriteLine(@"static void Main(string[] args)");
+                writer.WriteLine(@"{");
+                writer.WriteLine(@"{0} result = resolve();", GetTypeRepresentation(_args.ReturnType));
+                writer.WriteLine(@"Console.WriteLine(result);");
+                writer.WriteLine(@"return 0;");
+                writer.WriteLine(@"}");
+
+                writer.WriteLine(@"}");
+                writer.WriteLine(@"}");
+            }
+        }
+
+        private string GetTypeRepresentation(VariableType type)
+        {
+            switch (_args.ReturnType)
+            {
+                case VariableType.Void:
+                    throw new InvalidOperationException("The type Void cannot be returned");
+                case VariableType.Integer:
+                    return "int";
+                case VariableType.Double:
+                    return "double";
+            }
+
+            throw new InvalidOperationException(string.Format("No suitable type representation for {0}", type));
+        }
+    }
+}
