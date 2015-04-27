2c9417d3ad59b151ac090c0ff96f283946da4150
patryk <patrykbuzowicz@gmail.com>
2015-04-27 20:22:52 +0200

    web api basic support

---
+++ Sandbox.WebApi/Controllers/OperationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using Sandbox.Contracts;
+using Sandbox.Sandbox;
+using Sandbox.WebApi.Models;
+
+namespace Sandbox.WebApi.Controllers
+{
+    public class OperationController : BaseController
+    {
+        private ISandbox _sandbox = Manager.GetSandbox(false);
+
+        static OperationController()
+        {
+            Mapper.CreateMap<Input, EnvironmentInput>();
+            Mapper.CreateMap<EnvironmentOutput, Output>();
+        }
+
+        public Output Post(Input input)
+        {
+            EnvironmentInput envInput = Mapper.Map<EnvironmentInput>(input);
+            
+            if (string.IsNullOrEmpty(envInput.PackageName))
+            {
+                envInput.PackageName = "test";
+            }
+
+            EnvironmentOutput output = _sandbox.Run(envInput);
+
+            return Mapper.Map<Output>(output);
+        }
+    }
+}
---
+++ Sandbox.WebApi/Models/Input.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Sandbox.Contracts;
+using Sandbox.Contracts.Code;
+
+namespace Sandbox.WebApi.Models
+{
+    public class Input
+    {
+        [Required]
+        public PlatformType Platform { get; set; }
+
+        public string PackageName { get; set; }
+        
+        public IEnumerable<string> Libraries { get; set; }
+
+        [Required]
+        public VariableType ReturnType { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+    }
+}
