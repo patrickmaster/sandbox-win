569eb6c3ff7572f4bded4ec5bfe046005043f346
patryk <patrykbuzowicz@gmail.com>
2015-04-29 17:15:59 +0200

    refactor fix

---
+++ Sandbox.Contracts/Types/EnvironmentOutput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sandbox.Contracts.Types
+{
+    public class EnvironmentOutput
+    {
+        public Exception Exception { get; set; }
+
+        public string Result { get; set; }
+    }
+}
