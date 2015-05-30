a2a53b9cfd9d0ed86ed1cb7d671f3b8e85cddf0d
KTraczyk <traczyk92@gmail.com>
2015-05-30 16:18:26 +0200

    Scripts for DB
    Scripts to create database tables

---
+++ Sandbox.Database/dbo.Libraries.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Libraries] (
+    [Name]     VARCHAR (50) NOT NULL PRIMARY KEY,
+    [Path]     TEXT         NOT NULL,
+    [TaskId] INT     NOT NULL FOREIGN KEY REFERENCES Tasks(Id)
+);
+
