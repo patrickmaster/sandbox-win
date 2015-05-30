5565bc6299f8fdd0f8a210762cb864efa3e78d1b
KTraczyk <traczyk92@gmail.com>
2015-05-30 16:33:18 +0200

    Scripts for DB n-n version
    Scripts to create tables with n-n relationship

--- Sandbox.Database/dbo.Libraries.sql
+++ Sandbox.Database/dbo.Libraries.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [dbo].[Libraries] (
     [Name]     VARCHAR (50) NOT NULL PRIMARY KEY,
-    [Path]     TEXT         NOT NULL,
-    [TaskId] INT     NOT NULL FOREIGN KEY REFERENCES Tasks(Id)
+    [Path]     TEXT         NOT NULL
 );
 
